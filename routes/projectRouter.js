145e79b39f9954b9fc2859c5ba8449275563eb21
Dexter Lewis <dexter.lewis@gmail.com>
2019-05-19 21:27:56 -0400

    add get projects

---
+++ routes/projectRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dbProjects = require('../data/helpers/projectModel');
+const dbActions = require('../data/helpers/actionModel')
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const projects = await dbProjects.get();
+    res.status(200).json(projects);
+  } catch (err) {
+    res.status(500).json({ error: 'Unable to process request' });
+  }
+});
+
+// Middleware
+
+// async function validaiteAction (req, res, next) {
+//     try{
+//         const { id } = req.params
+//         const action = await dbActions.insert(id)
+//         if(action.length) {
+//             req.ac
+//         }
+//     }
+// }
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const projectsRouter = require('./routes/projectRouter')
 const server = express();
 
+server.use(express.json())
 
+// Routes
+server.use('/api/projects', projectsRouter)
+
+server.get('/', (req, res) => {
+    res.send('Server is running');
+})
 
 
 
